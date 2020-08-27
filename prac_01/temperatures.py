9f49df2ccbbe9b631e906089be7abb1601496474
DGMGIT <daniel.mackenzie1@my.jcu.edu.au>
2020-08-27 15:45:50 +1000

    Prac_01_github

--- prac_01/temperatures.py
+++ prac_01/temperatures.py
@@ -3,32 +3,22 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
-# MENU = """C - Convert Celsius to Fahrenheit
-# F - Convert Fahrenheit to Celsius
-# Q - Quit"""
-# print(MENU)
-# choice = input(">>> ").upper()
-# while choice != "Q":
-#     if choice == "C":
-#         celsius = float(input("Celsius: "))
-#         fahrenheit = celsius * 9.0 / 5 + 32
-#         print("Result: {:.2f} F".format(fahrenheit))
-#     elif choice == "F":
-#         fahrenheit = float(input("Fahrenheit: "))
-#         celsius = 5 / 9 * (fahrenheit - 32)
-#         print("Result: {:.2f} C".format(celsius))
-#     else:
-#         print("Invalid option")
-#     print(MENU)
-#     choice = input(">>> ").upper()
-# print("Thank you.")
-
-nums1 = [1, -5, 2, 0, 4, 2, -3]
-nums2 = [1, -5, 2, 4, 4, 2, 7]
-result = 0
-j = 0
-while j < len(nums1):
-    if nums1[j] != nums2[j]:
-        result = result + 1
-    j = j + 1
-print(result)
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print("Result: {:.2f} F".format(fahrenheit))
+    elif choice == "F":
+        fahrenheit = float(input("Fahrenheit: "))
+        celsius = 5 / 9 * (fahrenheit - 32)
+        print("Result: {:.2f} C".format(celsius))
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
