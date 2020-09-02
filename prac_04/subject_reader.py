76cb7ef408d0467ec71e4ec31b48c3934d059b55
DGMGIT <daniel.mackenzie1@my.jcu.edu.au>
2020-09-03 07:49:54 +1000

    gitignore

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -10,10 +10,12 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    print_subject(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    parts_list = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -24,7 +26,14 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        parts_list.append(parts)
     input_file.close()
+    return parts_list
+
+
+def print_subject(data):
+    for subject_data in data:
+        print("{} is taught by {:12} and has {:3} students".format(*subject_data))
 
 
 main()
