b4c57020e5c26102154a8bfb65409987ae6eb7a3
DGMGIT <daniel.mackenzie1@my.jcu.edu.au>
2020-10-23 00:25:20 +1100

    prac_09_github

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -9,13 +9,13 @@ PARENT_DIRECTORY = 'Lyrics'
 
 
 def main():
-
+    """takes file names and renames them based on get_fixed_filename """
     os.chdir(PARENT_DIRECTORY)
     for directory_name, subdirectories, filenames in os.walk('.'):
-        # print("Directory:", directory_name)
-        # print("\tcontains subdirectories:", subdirectories)
-        # print("\tand files:", filenames)
-        # print("(Current working directory is: {})".format(os.getcwd()))
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
 
         for filename in filenames:
             new_name = get_fixed_filename(filename)
@@ -28,10 +28,18 @@ def main():
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    enumerate(filename)
-    print(filename)
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
+    previous_char = ""
+    new_new_name = ""
+    for char in new_name:
+        if char.isupper():
+            if previous_char.isalpha():
+                if previous_char.islower:
+                    new_new_name += "_"
+        previous_char = char
+        new_new_name += char
+
+    return new_new_name
 
 
 main()
